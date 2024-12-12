35cf07702c2e9d954d86d2fbd69197877ba9bdac
vvskchaitanya <vvskchaitanya@gmail.com>
2024-12-12 18:51:53 +0000

    serve_ui

--- main/server.js
+++ main/server.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const fs = require("fs");
+const path = require('path');
 
 
 serve=function(){
@@ -7,7 +8,7 @@ serve=function(){
         let url = req.url;
         console.log("Request URL: "+req.url);
         if(url.indexOf("/ui/")==0){
-          console.log("hit ui");
+          serve_ui(req.url,res);
         }
         else if(url.indexOf("/api/")==0){
           console.log("hit api");
@@ -26,6 +27,51 @@ serve=function(){
         }  
     });
     server.listen(9999); 
+}
+
+serve_ui=function(url,res){
+  const filePath = path.join('ui', url.replace('/ui/', ''));
+  console.log(filePath);
+
+    // Get the file extension
+    const ext = path.extname(filePath).toLowerCase();
+
+    // Set the appropriate MIME type
+    const mimeTypes = {
+      '.html': 'text/html',
+      '.css': 'text/css',
+      '.js': 'application/javascript',
+      '.png': 'image/png',
+      '.jpg': 'image/jpeg',
+      '.jpeg': 'image/jpeg',
+      '.gif': 'image/gif',
+      '.svg': 'image/svg+xml',
+      '.json': 'application/json',
+      '.txt': 'text/plain',
+    };
+
+    const mimeType = mimeTypes[ext] || 'application/octet-stream';
+
+    // Check if the file exists
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+        return;
+      }
+
+      // Read and serve the file
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('500 Internal Server Error');
+          return;
+        }
+
+        res.writeHead(200, { 'Content-Type': mimeType });
+        res.end(data);
+      });
+    });
 }
 
 module.exports = { serve };
