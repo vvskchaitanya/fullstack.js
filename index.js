be674714ffbd89ff2e8b2f6bff686f52f1a5c753
Chaitanya V.V.S.K <vvskchaitanya@gmail.com>
2024-12-18 00:22:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-const http = require("http");
-const fs = require("fs");
-const URL = require("url");
-const crypto = require('crypto');
 var compiler = require('./main/compiler');
 var server = require('./main/server');
-
 
 // global variables
 const algorithm = "aes256";
 const options = { encoding: "UTF-8" };
 
-
 const timestamp = function(){
     return new Date().toUTCString();
 }
 
-api=function(req, api, res){
-    var func = fs.readFileSync(api,options);
-    var response = {status:false};
-    try{
-        response = eval(func);
-    }catch(err){
-        console.error(err);
-        response = {success:false, code:500, error:"Service Error"};
-    }
-    return response;
-}
+
 
 start = function(){
     compiler.compile();
