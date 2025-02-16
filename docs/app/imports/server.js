6bf06e1ab7bd45fc40ec5b376c29e47eb2ae6625
vvskchaitanya <vvskchaitanya@gmail.com>
2025-02-16 07:35:32 +0000

    up

--- docs/app/imports/server.js
+++ docs/app/imports/server.js
@@ -3,9 +3,12 @@ const fs = require("fs");
 const path = require('path');
 const { error } = require("console");
 
-const TARGET_UI = "target/ui";
-
-const TARGET_API = "target/ui";
+const UI_TARGET = "../target/ui";
+const API_TARGET = "../target/api";
+const UI_PORT = 9999;
+const API_PORT = 8888;
+const UI_PATH = "";
+const API_PATH = "";
 
 const MIME_TYPES = {
   '.html': 'text/html',
@@ -20,13 +23,9 @@ const MIME_TYPES = {
   '.txt': 'text/plain',
 };
 
-
-serve=function(){
-  serve_ui(9999,TARGET_UI,"ui");
-  serve_api(8888,TARGET_API,"api");
+serve = function(){
+  return [serve_ui(UI_PORT,UI_TARGET,UI_PATH),serve_api(API_PORT,API_TARGET,API_PATH)];
 }
-
-
 
 function serve_ui(port, target_dir, base_context){
   if(port==null){
@@ -37,7 +36,7 @@ function serve_ui(port, target_dir, base_context){
   }
   const server= http.createServer((req,res)=>{
     let url = req.url;
-    console.log("Request URL: "+req.url);
+    console.log("UI: Request URL: "+req.url);
     if(base_context!=null && base_context!=""){
       if(base_context[0]!="/")base_context="/"+base_context;
       if(base_context[base_context.lenth-1]!="/") base_context+="/";
@@ -45,28 +44,32 @@ function serve_ui(port, target_dir, base_context){
     }
     const filePath = path.join(target_dir, url);
     fs.stat(filePath, (err, stats) => {
-      if (err) {
-          res_writeFolder("ui",res);
+      if (err || stats==null) {
+          res_writeFolder(target_dir,res);
       }
-      if (stats.isDirectory()) {
+      else if (stats.isDirectory()) {
           res_writeFolder(filePath,res);
-      } else if (stats.isFile()) {
+      }
+      else if (stats.isFile()) {
           res_writeFile(filePath,res);
       }
     });
   });
-  try{
-    server.listen(port);
+   // Listen to port
+   server.listen(port,()=>{
     console.log("Serving UI - http://localhost:"+port);
-  }catch(err){
-    console.error("Unable to serve on "+PORT+". Port already in use.")
-  }
-  
+  });
+  server.on('error',(err)=>{
+    console.error("Unable to serve on "+port+". Port already in use.")
+  });
+  return server;
 }
 
 function serve_api(port, target_dir, base_context){
   const server = http.createServer((req, res) => {
     try {
+      let url = req.url;
+        console.log("API: Request URL: "+req.url);
         // Parse the context path from the URL
         const contextPath = new URL(url, `http://${req.headers.host}`).pathname;
 
@@ -80,7 +83,7 @@ function serve_api(port, target_dir, base_context){
         // Check if the file exists
         if (!fs.existsSync(filePath)) {
             res.statusCode = 404;
-            res.end(`API file ${fileName} not found.`);
+            res.end(`API ${url} not found.`);
             return;
         }
 
@@ -97,23 +100,28 @@ function serve_api(port, target_dir, base_context){
         res.end(`Error processing API request: ${error.message}`);
     }
   });
-  try{
-    server.listen(port);
+  // Listen to port
+  server.listen(port,()=>{
     console.log("Serving API - http://localhost:"+port);
-  }catch(err){
+  });
+  server.on('error',(err)=>{
     console.error("Unable to serve on "+port+". Port already in use.")
-  }
+  });
+  return server;
 }
 
 res_writeFolder = function(folder,res){
   if(folder[folder.length-1]!="/")folder+="/"
   fs.readFile(folder+"index.html", (err, data) => {
-    if (err) {
+    if (err || data==null) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
-        return res.end('500 Internal Server Error');
+        res.end('500 Internal Server Error');
+    }else{
+        res.writeHead(200, { 'Content-Type': "text/html" });
+        res.end(data);
+
     }
-    res.writeHead(200, { 'Content-Type': "text/html" });
-    res.end(data);
+    
   });
 }
 
